02d20f33811a1c5e04ce03599d8bb1ba58260dc4
Johnny Mnemonic <jm@machine-hall.org>
2015-08-18 18:08:04 +0200

    Added support for IBM Netfinity 5000 machines and their built-in Advanced System Management (ASM). Power on, power off and reset is supported.

---
+++ ibm/netfinity-5000/config/config.tcl
@@ -0,0 +1,9 @@
+# Serial port and configuration
+set _serialPort "/dev/ttyUSB0"
+set _serialBaudRate "57600"
+set _serialBits "cs8"
+
+# Credentials (factory default)
+set _username "USERID"
+set _password "PASSW0RD"
+
