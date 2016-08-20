db4f2716bbafee9f0633ac7d11454dc965c68a92
Johnny Mnemonic <jm@machine-hall.org>
2016-08-20 14:10:53 +0200

    The Scripts did not work when called from other directories than the directories the scripts were located. They now first determine the path they are installed in and prepend it to the path to the power-source when using the controls of the power-source. Also changed permissions to executable, so they can be used without further modification after cloning the repository.

--- misc/power-controllable-machine/controls/power-off.bash
+++ misc/power-controllable-machine/controls/power-off.bash
@@ -3,7 +3,11 @@
 _action="off"
 _pduPort="../config/power-source"
 
-${_pduPort}/controls/${_action}
+# Where am I really located?
+# Adapted from comments on http://stackoverflow.com/a/246128
+_scriptDir=$( dirname $( readlink -f "$0" ) )
+
+${_scriptDir}/${_pduPort}/controls/${_action}
 
 exit
 
