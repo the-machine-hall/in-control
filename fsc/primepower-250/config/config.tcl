e4908a45a975e2dbc0137b944295ecaa6c7e243b
Johnny Mnemonic <jm@machine-hall.org>
2017-01-23 16:21:30 +0100

    Added support for FSC PRIMEPOWER 250 machines.

---
+++ fsc/primepower-250/config/config.tcl
@@ -0,0 +1,8 @@
+# Configuration
+set _hostname "123.123.123.123"
+# The XSCF shell uses a telnet interface on TCP port 8010
+set _xscfShellPort "8010"
+
+# Credentials (Before use the password for the "root" account needs to be set via `madmin` which is part of the Enhanced Service Facility (ESF))
+set _username "root"
+set _password ""
